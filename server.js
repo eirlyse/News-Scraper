0cf059dd51aae555b02fcb78ddd799001d35b8a6
Eirlyse <eirlyse.s@gmail.com>
2020-01-04 09:52:15 -0700

    adding express, mongo,axios, cheerio

---
+++ server.js
@@ -0,0 +1,20 @@
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();
+
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
